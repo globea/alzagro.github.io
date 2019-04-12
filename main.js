01486344bf184f62494d656cda27143977ca3c27
gyopak <gyorgypakozdi@gmail.com>
2019-04-12 11:44:02 +0200

    feat: translation script, dev env fix

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log(`i'm wired`);
+const fetchTranslation = async lang => 
+    await (await fetch(`local/${lang}.json`)).json();
+
+const populateTranslation = dict => 
+  Object.keys(dict).forEach(t => {
+      document.querySelector(`#${t}`).innerText = dict[t];
+  });
+
+const translate = lang => {
+  fetchTranslation(lang)
+    .then(dict => populateTranslation(dict));
+};
+
+window.onload = _ => {
+  // plz work i don't have catch block
+  translate('English');
+}
