1a2025c24c0ff768de609f85dc39c4eb28054473
Blanka Hooz <hooz.blanka@gmail.com>
2019-04-13 21:52:03 +0200

    feat: add function to change header on scroll

--- main.js
+++ main.js
@@ -11,7 +11,18 @@ const translate = lang => {
     .then(dict => populateTranslation(dict));
 };
 
+const changeHeaderOnScroll = () => {
+  const height = document.querySelector('.main').clientHeight;
+  document.body.scrollTop > height || document.documentElement.scrollTop > height ?
+    document.querySelector('header').style.backgroundColor = "rgba(0, 0, 0, 1)" :
+    document.querySelector('header').style.backgroundColor = "rgba(0, 0, 0, 0)";
+}
+
 window.onload = _ => {
   // plz work i don't have catch block
   translate('English');
+}
+
+window.onscroll = _ => {
+  changeHeaderOnScroll();
 }
