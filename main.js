f204ad5bac44cee08a4b1b8f665151194963c67a
Blanka Hooz <hooz.blanka@gmail.com>
2019-04-13 22:14:50 +0200

    refactor: update values at which header changes

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const translate = lang => {
 
 const changeHeaderOnScroll = () => {
   const height = document.querySelector('.main').clientHeight;
-  document.body.scrollTop > height || document.documentElement.scrollTop > height ?
+  document.body.scrollTop > height - 1 || document.documentElement.scrollTop > height - 1 ?
     document.querySelector('header').style.backgroundColor = "rgba(0, 0, 0, 1)" :
     document.querySelector('header').style.backgroundColor = "rgba(0, 0, 0, 0)";
 }
