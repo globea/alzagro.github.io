c6fcf1f0fef3ab96dc8f36cc9e7953726307988e
gyopak <gyorgypakozdi@gmail.com>
2019-04-22 11:58:09 +0200

    fix: email partly works

--- main.js
+++ main.js
@@ -18,6 +18,19 @@ const changeHeaderOnScroll = () => {
     document.querySelector('header').style.backgroundColor = "rgba(0, 0, 0, 0)";
 }
 
+const loadSubscription = e => {
+  console.log(e);
+  // const button = document.querySelector('#real-signup-button');
+  // const work = document.querySelector('')
+  // button.addEventListener('click', _ => {
+  //   emailjs.send("mailjet", "template_kvgAjN6o", {
+  //     "work":"rewrew",
+  //     "email_address":"543543",
+  //     "name":"432432"
+  //   })
+  // });
+}
+
 window.onload = _ => {
   // plz work i don't have catch block
   translate('English');
