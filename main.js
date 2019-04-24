8016d3b95ec2cddf9c7dfb0c701668a944b543c1
Blanka Hooz <hooz.blanka@gmail.com>
2019-04-24 20:48:30 +0200

    feat: add changeLanguage function

--- main.js
+++ main.js
@@ -10,27 +10,41 @@ const translate = lang => {
   fetchTranslation(lang).then(dict => populateTranslation(dict));
 };
 
+const changeLanguage = _ => {
+  const languageButton = document.querySelector('#language_button');
+  languageButton.addEventListener('click', e => {
+    if (languageButton.innerText === 'HU') {
+      translate('Hungarian');
+      languageButton.innerText = 'ENG';
+    } else {
+      translate('English');
+      languageButton.innerText = 'HU';
+    }
+    e.preventDefault();
+  });
+};
+
 const changeHeaderOnScroll = () => {
-  const height = document.querySelector(".main").clientHeight;
+  const height = document.querySelector('.main').clientHeight;
   document.body.scrollTop > height - 1 ||
   document.documentElement.scrollTop > height - 1
-    ? (document.querySelector("header").style.backgroundColor =
-        "rgba(0, 0, 0, 1)")
-    : (document.querySelector("header").style.backgroundColor =
-        "rgba(0, 0, 0, 0)");
+    ? (document.querySelector('header').style.backgroundColor =
+        'rgba(0, 0, 0, 1)')
+    : (document.querySelector('header').style.backgroundColor =
+        'rgba(0, 0, 0, 0)');
 };
 
 const formListenerInit = _ => {
-  document.querySelector("#subscribe").addEventListener("submit", function(e) {
+  document.querySelector('#subscribe').addEventListener('submit', function(e) {
     e.preventDefault();
     if (e.isTrusted) {
-      emailjs.send("mailjet_alzagro", "template_kvgAjN6o", {
+      emailjs.send('mailjet_alzagro', 'template_kvgAjN6o', {
         name: e.target[0].value,
         email_address: e.target[1].value,
         work: e.target[2].innerText
       });
-      e.target.classList.toggle("hidden");
-      document.querySelector("#signup_success").classList.toggle("hidden");
+      e.target.classList.toggle('hidden');
+      document.querySelector('#signup_success').classList.toggle('hidden');
     }
     return false;
   });
@@ -38,8 +52,9 @@ const formListenerInit = _ => {
 
 window.onload = _ => {
   // plz work i don't have catch block
-  translate("English");
+  translate('English');
   formListenerInit();
+  changeLanguage();
 };
 
 window.onscroll = _ => {
